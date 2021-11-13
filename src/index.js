1f732ad6ca09a91e09ba9b4bf85cd279be575d27
Albert Takács <albitakacs@gmail.com>
2021-11-13 17:59:37 +0100

    finish task

--- src/index.js
+++ src/index.js
@@ -4,40 +4,74 @@ import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+const LENGTH = 10;
+
 const refs = {
     input: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
 };
+
 refs.input.addEventListener('input', debounce(onSearchCountries, DEBOUNCE_DELAY));
 
 function onSearchCountries(event) {
    const inputValue =  event.target.value;
-   const trimInputValue = inputValue.trim();
-   if(!trimInputValue){
+   const trimedInputValue = inputValue.trim();
+   
+   if(!trimedInputValue){
+    refs.countryList.innerHTML = "";
+    refs.countryInfo.innerHTML = "";
     return;
    }
 
-   fetchCountries(trimInputValue)
-   .then(d =>{
-       console.log(d);
-       if(d.length > 10){
-        Notify.info('Too many matches found. Please enter a more specific name.')
+   fetchCountries(trimedInputValue)
+   .then(data => {
+       console.log(data);
+        if(data.length > LENGTH){
+            Notify.info('Too many matches found. Please enter a more specific name.')
+            return;
+        }
         
-       }
-       showCountries(d);
+       const markup = renderCountries(data);
+       const cardMarkup = renderCountryCard(data);
+       if(data.length === 1){
+           
+           refs.countryInfo.innerHTML = cardMarkup;
+        } else {
 
-       
+            refs.countryList.innerHTML =  markup;
+        }
+        
    })
-   .catch(e => console.log(e));
+   .catch(showError);
 
 }
 
-function showCountries(countries) {
+function renderCountries(countries) {
+    refs.countryInfo.innerHTML = "";
+
   return countries.map(country => {
-    const markup = `<li class="country-list-item"><img src="${country.flags.svg}">${country.name.official}</li>`
-        console.log(markup);
+    return `<li class="country-list-item"><img class="country-flag-image" src="${country.flags.svg}">${country.name.official}</li>`
+        
     }).join('');
 }
 
-// [0].name.official
+function renderCountryCard(name) {
+    refs.countryList.innerHTML = "";
+
+    const countryLanguages = Object.values(name[0].languages);
+    return `
+    <ul class=country-card-list>
+    <li class="country-card-item"><img class="country-flag-image" src="${name[0].flags.svg}"><b class="country-card-desc">${name[0].name.official}</b></li>
+    <li class="country-card-item"><b>Capital</b>: ${name[0].capital}</li>
+    <li class="country-card-item"><b>Population</b>: ${name[0].population}</li>
+    <li class="country-card-item"><b>Languages</b>: ${countryLanguages}</li></ul>`
+}
+
+function showError() {
+    Notify.failure("Oops, there is no country with that name");
+    
+}
+
+
+
--- src/js/services/fech-countries.js
+++ src/js/services/fech-countries.js
@@ -2,7 +2,7 @@
 
 
 export function fetchCountries(countryName){
-    // const { capital, population, languages } = countryName;
+    
     return   fetch(`${BASE_URL}/${countryName}?fields=name,capital,population,flags,languages`).then(response => {
         if(!response.ok){
             
